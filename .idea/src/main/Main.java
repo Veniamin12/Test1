9cb8e4a2e8adbd61d9cd7bc498b31c6219af698c
Veniamin12 <ven4ik11@gmail.com>
2023-09-10 14:12:57 +0300

    3. Add Main.java

---
+++ .idea/src/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Main.getInt(5,12));
+    }
+    public static double getInt(int a , int b ){
+        double y =(a+b)/100 + 55;
+        return y;
+    }
+}
