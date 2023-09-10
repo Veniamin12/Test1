44d33f30ad3c618dc5a5b9edca1e2733bd748aac
Veniamin12 <ven4ik11@gmail.com>
2023-09-10 14:44:55 +0300

    5.hello from second

--- .idea/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Main.getInt(5,12));
-    }
-    public static double getInt(int a , int b ){
-        double y =(a+b)/100 + 55;
-        return y;
-    }
-}
