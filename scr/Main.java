560560071f19b61b18fb3982f23c7bcffe6384a0
Veniamin12 <ven4ik11@gmail.com>
2023-09-10 14:37:04 +0300

    4. Method sayG

---
+++ scr/Main.java
@@ -0,0 +1,13 @@
+package scr;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Main.getRes(5,13));
+    }
+    public static double getRes ( int a, int b){
+        return (double)(a+b)/100 +55;
+    }
+    public static void sayG(String u){
+        System.out.println("gdgdgd" + u);
+    }
+}
